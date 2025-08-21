ec91e577da15d8a4a69499f0a35296651ea72748
princetigr <princetigr@users.noreply.github.com>
2025-08-21 18:05:22 +0000

    Deploy project-x to GitHub Pages 46cb8b4858a887604493adc33b4de09ed4d9f73e

---
+++ project-x/src/App.jsx
@@ -0,0 +1,28 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Featured from "./pages/Featured";
+import Testimonial from "./pages/Testimonial";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/featured" element={<Featured />} />
+            <Route path="/testimonial" element={<Testimonial />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
