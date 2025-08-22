dce28bda67bcadf7e8783c40126ceb12a6568950
sakib078 <sakibmansuri044@gmail.com>
2025-08-22 11:20:35 -0400

    changes In a base url for vite.

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename="/promo-pages/project-x">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/promo-pages/project-x/',
   server: {
     port: 3000,
     open: true
