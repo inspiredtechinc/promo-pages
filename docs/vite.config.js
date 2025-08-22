69ee52c5ef7df5c9e9ac57b4ae3242fd080e785e
inspiredtech <work@inspiredtech.ca>
2025-08-22 02:36:35 -0400

    fix: copy dist folder to deployment directory for GitHub Pages

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project-x/', // Ensure assets load from the correct subfolder on GitHub Pages
   plugins: [react()],
   server: {
     port: 3000,
